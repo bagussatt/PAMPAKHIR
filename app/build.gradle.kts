dc065ba117d344a29f30be5a3be73bd7986aaf0a
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2024-01-10 20:58:18 +0700

    menambahkan function getAllData pada OutcomeViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/aplikasie_orange/data/OutcomeViewModel.kt
+++ app/src/main/java/com/example/aplikasie_orange/data/OutcomeViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.aplikasie_orange.data
 
-class OutcomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+class OutcomeViewModel : ViewModel() {
+    fun getAllData(): kotlinx.coroutines.flow.Flow<List<OutcomeData>> = callbackFlow {
+        val fireStoreRef = Firebase.firestore.collection("Outcome")
+
+        val subscription = fireStoreRef.addSnapshotListener { value, error ->
+            if (error != null) {
+                // Handle error
+                close(error)
+                return@addSnapshotListener
+            }
+
+            if (value != null) {
+                val dataList = mutableListOf<OutcomeData>()
+                for (doc in value.documents) {
+                    val outcomeData = doc.toObject(OutcomeData::class.java)
+                    if (outcomeData != null) {
+                        dataList.add(outcomeData)
+                    }
+                }
+                trySend(dataList)
+            }
+        }
+
+        // Cancellation callback
+        awaitClose {
+            subscription.remove()
+        }
+    }
+
 }
