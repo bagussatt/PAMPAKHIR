2ae4bbfa417290fb9be5180568ffaeffd2384943
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2024-01-10 20:44:09 +0700

    menambahkan class OutcomeData

---
+++ app/src/main/java/com/example/aplikasie_orange/data/OutcomeData.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasie_orange.data
+
+class OutcomeData (
+
+    var idOutme: String = "",
+    var date: String = "",
+    var uangmasuk: Double = 0.0,
+    var note: String = ""
+)
